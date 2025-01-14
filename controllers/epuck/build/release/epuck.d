2c53859ce2a07608f0fa9e31cf1e474c7cf70b73
sahas-eashan <sahaseashangalle@gmail.com>
2025-01-14 18:08:24 +0530

    dfd

--- controllers/epuck/build/release/epuck.d
+++ controllers/epuck/build/release/epuck.d
@@ -1,9 +1,9 @@
 build/release/epuck.o: epuck.cpp epuck.hpp \
- /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
- /usr/local/webots/include/controller/cpp/webots/LED.hpp \
- /usr/local/webots/include/controller/cpp/webots/Device.hpp \
- /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/LED.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
  sensor_manager.hpp \
- /usr/local/webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
  config.hpp motors.hpp \
- /usr/local/webots/include/controller/cpp/webots/Motor.hpp
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp
--- controllers/epuck/build/release/main.d
+++ controllers/epuck/build/release/main.d
@@ -1,7 +1,9 @@
 build/release/main.o: main.cpp epuck.hpp \
- /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
- /usr/local/webots/include/controller/cpp/webots/Motor.hpp \
- /usr/local/webots/include/controller/cpp/webots/Device.hpp \
- /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
- /usr/local/webots/include/controller/cpp/webots/DistanceSensor.hpp \
- /usr/local/webots/include/controller/cpp/webots/LED.hpp
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/LED.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ sensor_manager.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ config.hpp motors.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp
